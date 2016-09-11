a44d1aea3bd9aec771b7fd07486373b8adc26aaa
Ashloids <ashleyhb@hotmail.com>
2016-09-11 18:36:44 +0100

    Added placeholder work section

--- Portfolio-starterkit/assets/js/functions.js
+++ Portfolio-starterkit/assets/js/functions.js
@@ -1,6 +1,8 @@
 $(function() {
 
   smoothScroll(1000);
+  workBelt();
+  workLoad();
 
 });
 
@@ -16,5 +18,37 @@ function smoothScroll (duration) {
       }, duration);
 
     }
+  });
+}
+
+
+function workBelt() {
+
+  $('.thumb-unit').click(function(){
+
+    $('.work-belt').css('left', '-100%');
+    $('.work-container').show();
+  });
+
+  $('.work-return').click(function() {
+    $('.work-belt').css('left', '-0%');
+    $('.work-container').hide(400);
+  });
+
+}
+
+function workLoad() {
+
+  $.ajaxSetup ({ cache: true });
+
+  $('.thumb-unit').click(function() {
+
+    var $this = $(this),
+        newTitle = $this.find('strong').text(),
+        newfolder = $this.data('folder'),
+        spinner = '<div class="loader">Loading...</div>',
+        newHTML = '/work/'+ newfolder +'.html';
+    $('.project-load').html(spinner).load(newHTML);
+    $('.project-title').text(newTitle);
   });
 }
